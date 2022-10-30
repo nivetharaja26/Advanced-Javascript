e9d2cad40d62e93172a8a713c491987242a001c0
NIVETHA RAJA <77013723+nivetharaja26@users.noreply.github.com>
2022-10-30 16:37:10 +0530

    Curry function

--- Curry.js
+++ Curry.js
@@ -17,6 +17,16 @@ function curry(fn)
     }
 }
 
+
 const cursum=curry(sum)
 
+/*
 console.log(cursum(3)(4)(5))
+
+*/
+
+const add2=cursum(3)
+const add3=add2(4)
+const add4=add3(5)
+
+console.log(add4)
